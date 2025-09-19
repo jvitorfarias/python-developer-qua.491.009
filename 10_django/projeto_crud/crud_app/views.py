d7b2c1c7d6825d2955c0a01a644f5b4762943eee
jvitorfarias <joaovitorfarias4@gmail.com>
2025-09-18 21:44:36 -0300

    commit da aula do dia 18/09/2025, adicionando pagina de cadastro

--- 10_django/projeto_crud/crud_app/views.py
+++ 10_django/projeto_crud/crud_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pessoa
 
 # Create your views here.
@@ -6,3 +6,12 @@ def home(request):
     pessoas = Pessoa.objects.all()
 
     return render(request, "home.html", {"pessoas":pessoas})
+
+def cadastro_pessoa(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        Pessoa.objects.create(nome=nome, email=email, cpf=cpf)
+        return redirect('home')
+    return render(request, 'cadastrar.html')
